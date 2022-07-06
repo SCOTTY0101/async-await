c6f2e1387f4e20acf2a2baf25526be8eeef9a9bb
SCOTTY0101 <alan777555@hotmail.com>
2022-07-06 21:15:26 +1000

    vim text editor update

--- async-await.js
+++ async-await.js
@@ -1,9 +1,13 @@
 //Async and await function that returns a promise.
-//Vim text editor. i: insert, esc:w write, ecs:q! discard all changes and quit
+
+//Vim text editor. i: insert, esc:w write, :q quit OR  ecs:q! discard all changes and quit
+
+//git. git init, git add <files> or . <all files>, git commit -m "your save message",
+// git push -u origin maim
 
     const doSomethingAsync = () => {
         return new Promise(resolve => {
-            setTimeout(() => resolve(alert('I waited 4 seconds before responding....')), 4000);
+            setTimeout(() => resolve( alert('I waited 5 seconds before responding....') ), 5000);
         });
     };
 
@@ -13,7 +17,7 @@
 
 /*
 *Calls the function doSomething, which calls doSomethingAsync.
-*Timeout is set to 4 seconds before the alert box is triggered.
+*Timeout is set to 5 seconds before the alert box is triggered.
 */ 
    console.log(doSomething());
 
